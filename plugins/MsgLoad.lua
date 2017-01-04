271e85b63af28f1fc13305463e6ffdfe8df2b595
/Seyed <seyedkia27@gmail.com>
2017-01-04 13:39:36 +0330

    Update MsgLoad.lua

--- plugins/MsgLoad.lua
+++ plugins/MsgLoad.lua
@@ -176,7 +176,7 @@ local hash10 = 'strict:'..msg.to.id
 						kick_user(msg.from.id, msg.to.id)
 					end
 				end
-				if lock_sticker == "yes" and msg.media.caption:match("sticker.webp") then
+				if redis:get(hash8) and msg.media.caption:match("sticker.webp") then
 					delete_msg(msg.id, ok_cb, false)
 					if strict == "yes" or to_chat then
 						kick_user(msg.from.id, msg.to.id)
