0cff3439610ff6f0574b8af0495fa40e3ea87c5c
/Seyed <seyedkia27@gmail.com>
2017-01-05 14:33:45 +0330

    Update InSuper.lua

--- plugins/InSuper.lua
+++ plugins/InSuper.lua
@@ -1825,8 +1825,30 @@ local function run(msg, matches)
 				resolve_username(username,  callbackres, cbres_extra)
 			else
 				savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested SuperGroup ID")
-                    local text = "شناسه شما : <b> ["..msg.from.id.."] </b>\nشناسه گروه : <b> ["..msg.to.id.."</b>]"
+local user_id = msg.from.id
+local chat_id = get_receiver(msg)
+local token = "236823773:AAHuvA1wudU3hStv2Qq4RjD-MtSZoiTRPf4"
+local db = 'https://api.telegram.org/bot'..token..'/getUserProfilePhotos?user_id='..user_id
+local path = 'https://api.telegram.org/bot'..token..'/getFile?file_id='
+local img = 'https://api.telegram.org/file/bot'..token..'/'
+local res, code = https.request(db)
+local jdat = json:decode(res)
+local fileid = jdat.result.photos[1][3].file_id
+local count = jdat.result.total_count
+if not tonumber(count) == 0 then
+local pt, code = https.request(path..fileid)
+local jdat2 = json:decode(pt)
+local path2 = jdat2.result.file_path
+local link = img..path2
+local photo = download_to_file(link,"ax"..user_id..".jpg")
+local textp = "شناسه شما : <b> ["..msg.from.id.."] </b>\nشناسه گروه : <b> ["..msg.to.id.."</b>]\nنام شما : <b>"..msg.from.first_name.." </b>\nتعداد عکس های شما : <b>"..count.." </b>\n"					
+send_photo2(chat_id, photo, text[,ok_cb, false)	
+else
+					
+local text = "شناسه شما : <b> ["..msg.from.id.."] </b>\nشناسه گروه : <b> ["..msg.to.id.."</b>]\nنام شما : <b>"..msg.from.first_name.." </b>\n"
                     return reply_msg(msg.id,text,ok_cb,false)
+end
+end				
 			end
 		end
 
