e480aa2ab8aace9f915324c86852c2101a324d37
/Seyed <seyedkia27@gmail.com>
2017-01-05 16:04:57 +0330

    Update Info.lua

--- plugins/Info.lua
+++ plugins/Info.lua
@@ -1,11 +1,10 @@
 function run(msg,matches , result)
   local user_id = msg.from.id
   local chat_id = get_receiver(msg)
-  local token = "236823773:AAHuvA1wudU3hStv2Qq4RjD-MtSZoiTRPf4"
   local db = 'https://api.telegram.org/bot123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11/getMe'
   local res, code = https.request(db)
   local jdat = json:decode(res)
-    return jdat
+    send_large_msg(chat_id, jdat)
 end
 return {
   patterns = {
