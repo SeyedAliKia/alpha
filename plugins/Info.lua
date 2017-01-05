303eed167d2fe6bb9bff53960336b876258feefd
/Seyed <seyedkia27@gmail.com>
2017-01-05 18:04:52 +0330

    Update Info.lua

--- plugins/Info.lua
+++ plugins/Info.lua
@@ -1,93 +1,32 @@
-local function run(msg, matches)
-  
-  if matches[1]:lower() == 'id' and not msg.reply_id then
-    local token = '236823773:AAHuvA1wudU3hStv2Qq4RjD-MtSZoiTRPf4'
-    local user_id = msg.from.id
-    local chat_id = get_receiver(msg)
-    local db = 'https://api.telegram.org/bot'..token..'/getUserProfilePhotos?user_id='..user_id
-    local path = 'https://api.telegram.org/bot'..token..'/getFile?file_id='
-    local img = 'https://api.telegram.org/file/bot'..token..'/'
-    local res, code = https.request(db)
-    local jdat = json:decode(res)
-    local fileid = ''
-    local count = jdat.result.total_count
-    local bot = '223667070'
-    if jdat.result.total_count == 0 then
-      send_photo(chat_id, photos,ok_cb,false)
-    else
-      fileid = jdat.result.photos[1][3].file_id
-    end
-    if tonumber(count) == 0 then
-      local photos = '/data/photos/ax.jpg'
-      local user_info = {}
-      local uhash = 'user:'..msg.from.id
-      local user = redis:hgetall(uhash)
-      local um_hash = 'msgs:'..msg.from.id..':'..msg.to.id
-      user_info.msgs = tonumber(redis:get(um_hash) or 0)
-      local text = '》 نام : {'..msg.from.first_name..'}\n'
-      ..'》 شناسه کاربری : ['..msg.from.id..']\n'
-      ..'》 یوزرنیم [ @'..(msg.from.username or '—-')..' ]\n'
-      ..'》 تعداد عکس پروفایل : ['..count..']\n'
-      ..'》 تعداد پیام ارسال شده : ['..user_info.msgs..']'
-      send_photo2(chat_id,photos,text,ok_cb,false)
-    else
-      local pt, code = https.request(path..fileid)
-      local jdat2 = json:decode(pt)
-      local path2 = jdat2.result.file_path
-      local link = img..path2
-      local photo = download_to_file(link,"ax"..user_id..".jpg")
-      local user_info = {}
-      local uhash = 'user:'..msg.from.id
-      local user = redis:hgetall(uhash)
-      local um_hash = 'msgs:'..msg.from.id..':'..msg.to.id
-      user_info.msgs = tonumber(redis:get(um_hash) or 0)
-      local hash = 'rank:variables'
-      if hash then
-        local value = redis:hget(hash, msg.from.id)
-        --if not value then
-          -- value = value..'---'
-          -- end
-        end
-        local _nl, ctrl_chars = string.gsub(msg.text, '%c', '')
-        if string.len(msg.from.print_name) > 40 or ctrl_chars > 70 then
-          local text = '<i> 》کاربر </i> [<b>'..msg.from.first_name..'</b>] <i> به دلیل استفاده از کارکتر های طولانی در نام اکانت کاربری خود قادر به نمایش اطلاعات شما نیستم </i>'
-          return reply_msg(msg.id, text, ok_cb, false)
-        end
-          userrank = "Member"
-          if tonumber(msg.from.id) == 159887854 then
-            userrank = "Master ⭐⭐⭐⭐"
-          elseif is_sudo(msg) then
-            userrank = "Sudo ⭐⭐⭐⭐⭐"
-          elseif is_admin1(msg) then
-            userrank = "Admin ⭐⭐⭐"
-          elseif tonumber(msg.from.id) == tonumber(is_owner) then
-            userrank = "Leader ⭐⭐"
-          elseif is_momod(msg) then
-            userrank = "Moderator ⭐"
-          elseif not is_momod(msg) then
-            userrank = "Member ⭐"
-          elseif tonumber(msg.from.id) == tonumber(our_id) then
-            userrank = "SecurityBOT ⭐⭐⭐⭐⭐⭐"
-          elseif msg.from.username then
-            if string.sub(msg.from.username:lower(), -3) == "bot" then
-              userrank = "API Bot"
-            end
-          end
-            local text = '》 نام : {'..msg.from.first_name..'}\n'
-            ..'》 شناسه کاربری: ['..msg.from.id..']\n'
-            ..'》 یوزرنیم: [ @'..(msg.from.username or '—-')..' ]\n'
-            ..'》 تعداد تصاویر پروفایل: ['..count..']\n'
-            ..'》 تعداد پیام های ارسالی: ['..user_info.msgs..']\n'
-            ..'》 مقام: [---]\n'
-            ..'》 جایگاه: ['..userrank..']'
-            send_photo2(chat_id, photo, text, ok_cb, false)
-        end
-      end
-    end
-
-    return {
-      patterns = {
-        "^[#!/]([Ii][Nn][Ff][Oo][Mm][Ee])$"
-      },
-      run = run
-    }
+function run(msg,matches , result)
+  local user_id = msg.from.id
+  local chat_id = get_receiver(msg)
+  local token = "236823773:AAHuvA1wudU3hStv2Qq4RjD-MtSZoiTRPf4"
+  local db = 'https://api.telegram.org/bot'..token..'/getUserProfilePhotos?user_id='..user_id
+  local path = 'https://api.telegram.org/bot'..token..'/getFile?file_id='
+  local img = 'https://api.telegram.org/file/bot'..token..'/'
+  local res, code = https.request(db)
+  local jdat = json:decode(res)
+  local count = jdat.result.total_count
+  if tonumber(count) == 0 then
+    local photo = '/data/photos/ax.jpg'
+    local textp = "شناسه شما : ["..msg.from.id.."]\nشناسه گروه : ["..msg.to.id.."]\nنام شما : "..msg.from.first_name.."\nتعداد عکس های شما :"..count.."\n"					
+    send_photo2(chat_id, photo, textp, ok_cb, false)  
+  elseif tonumber(count) ~= 0 then
+    local fileid = jdat.result.photos[1].file_id
+    local pt, code = https.request(path..fileid)
+    local jdat2 = json:decode(pt)
+    local path2 = jdat2.result.file_path
+    local link = img..path2
+    local photo = download_to_file(link,"ax"..user_id..".jpg")
+    local textp = "شناسه شما : ["..msg.from.id.."]\nشناسه گروه : ["..msg.to.id.."]\nنام شما : "..msg.from.first_name.."\nتعداد عکس های شما :"..count.."\n"					
+    send_photo2(chat_id, photo, textp, ok_cb, false)
+    return
+  end
+end
+return {
+  patterns = {
+    "^(infome)$",
+  },
+  run = run
+}
