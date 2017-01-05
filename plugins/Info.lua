80966684269a46e0f5d31f0ff399d9c60b0e6008
/Seyed <seyedkia27@gmail.com>
2017-01-05 17:10:23 +0330

    Update Info.lua

--- plugins/Info.lua
+++ plugins/Info.lua
@@ -1,24 +1,21 @@
-function run(msg,matches , result)
-  local user_id = msg.from.id
-  local chat_id = get_receiver(msg)
-  local token = "236823773:AAHuvA1wudU3hStv2Qq4RjD-MtSZoiTRPf4"
-  local db = 'https://api.telegram.org/bot'..token..'/getUserProfilePhotos?user_id='..user_id
-  local path = 'https://api.telegram.org/bot'..token..'/getFile?file_id='
-  local img = 'https://api.telegram.org/file/bot'..token..'/'
+function run(msg, matches, result)
+  local db = 'https://api.telegram.org/bot236823773:AAHuvA1wudU3hStv2Qq4RjD-MtSZoiTRPf4/getUserProfilePhotos?user_id='..msg.from.id
+  local path = 'https://api.telegram.org/bot236823773:AAHuvA1wudU3hStv2Qq4RjD-MtSZoiTRPf4/getFile?file_id='
+  local img = 'https://api.telegram.org/file/bot236823773:AAHuvA1wudU3hStv2Qq4RjD-MtSZoiTRPf4/'
   local res, code = https.request(db)
   local jdat = json:decode(res)
   local fileid = jdat.result.photos[1][3].file_id
   local count = jdat.result.total_count
   if tonumber(count) == 0 then
-    send_large_msg(chat_id,"Image Not Found",ok_cb,false)
+    reply_msg(msg.id, "شناسه شما : <b> ["..msg.from.id.."] </b>\nشناسه گروه : <b> ["..msg.to.id.."</b>]\nنام شما : <b>"..msg.from.first_name.." </b>\n", ok_cb, false)
   else
     local pt, code = https.request(path..fileid)
     local jdat2 = json:decode(pt)
     local path2 = jdat2.result.file_path
     local link = img..path2
-    local photo = download_to_file(link,"ax"..user_id..".jpg")
-    local textp = "شناسه شما : <b> ["..msg.from.id.."] </b>\nشناسه گروه : <b> ["..msg.to.id.."</b>]\nنام شما : <b>"..msg.from.first_name.." </b>\nتعداد عکس های شما : <b>"..count.." </b>\n"					
-    send_photo2(chat_id, photo, textp,ok_cb, false)	
+    local photo = download_to_file(link, "ax"..msg.from.id".jpg")
+    local textp = "شناسه شما : ["..msg.from.id.."]\nشناسه گروه : ["..msg.to.id.."]\nنام شما : "..msg.from.first_name.."\nتعداد عکس های شما :"..count.."\n"					
+    send_photo2(get_receiver(msg), photo, textp,ok_cb, false)	
     return
   end
 end
