7e760ce341eb07f23de658937375275dadcb1f19
/Seyed <seyedkia27@gmail.com>
2017-01-06 15:27:54 +0330

    Update Voice.lua

--- plugins/Voice.lua
+++ plugins/Voice.lua
@@ -2,7 +2,7 @@ function run(msg, matches)
 
   if matches[1]:lower() == "voice" then
     if string.len(matches[2]) > 20 then
-      return reply_msg(msg.id, "❌ تعداد کاراکتر ها باید کمتر از 20 باشد !", ok_cb, false)
+      return reply_msg(msg.id, "❌ <i>تعداد کاراکتر ها باید کمتر از 20 باشد ! </i>", ok_cb, false)
     else
       local url = "https://irapi.ir/aryana/api.php?text="..matches[2]
       local file = download_to_file(url, 'voice.ogg')
