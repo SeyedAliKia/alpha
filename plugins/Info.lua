187702a1b49e8d9f750458eb302c590ddb00d412
/Seyed <seyedkia27@gmail.com>
2017-01-09 17:45:53 +0330

    Update Info.lua

--- plugins/Info.lua
+++ plugins/Info.lua
@@ -2,15 +2,15 @@ do
   local function run(msg, matches)
     if matches[1]:lower() == "info" then
       if is_sudo(msg) or is_admin1(msg) then
-        reply_document(msg.id, "./dat/photos/sudo.webp", ok_cb, false)
+        reply_document(msg.id, "./data/photos/sudo.webp", ok_cb, false)
       end
       if is_owner(msg) then
         reply_document(msg.id, "./data/photos/owner.webp", ok_cb, false)
       end
       if is_momod(msg) then
-        reply_document(msg.id, "./data/me/momod.webp", ok_cb, false)
+        reply_document(msg.id, "./data/photos/momod.webp", ok_cb, false)
       else
-        reply_document(msg.id, "./data/me/member.webp", ok_cb, false)
+        reply_document(msg.id, "./data/photos/member.webp", ok_cb, false)
       end
     end
   end
