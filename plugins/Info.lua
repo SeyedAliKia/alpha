049c31c824de8764593488b5ddfbf3f16b050253
/Seyed <seyedkia27@gmail.com>
2017-01-09 17:43:57 +0330

    Update Info.lua

--- plugins/Info.lua
+++ plugins/Info.lua
@@ -1,13 +1,26 @@
 do
   local function run(msg, matches)
-
+    if matches[1]:lower() == "info" then
+      if is_sudo(msg) or is_admin1(msg) then
+        reply_document(msg.id, "./dat/photos/sudo.webp", ok_cb, false)
+      end
+      if is_owner(msg) then
+        reply_document(msg.id, "./data/photos/owner.webp", ok_cb, false)
+      end
+      if is_momod(msg) then
+        reply_document(msg.id, "./data/me/momod.webp", ok_cb, false)
+      else
+        reply_document(msg.id, "./data/me/member.webp", ok_cb, false)
+      end
+    end
   end
+end
 
-  return {
-    patterns = {
-      "^([Ii][Nn][Ff][Oo])$",
-    },
-    run = run
-  }
+return {
+  patterns = {
+    "^([Ii][Nn][Ff][Oo])$",
+  },
+  run = run
+}
 
 end
