13fd67d0d926042c18947f912e61016e2808001c
/Seyed <seyedkia27@gmail.com>
2017-01-09 19:05:41 +0330

    Update Info.lua

--- plugins/Info.lua
+++ plugins/Info.lua
@@ -2,10 +2,10 @@ local function run(msg, matches)
   if matches[1]:lower() == "info" then
     local name = msg.from.first_name
     if msg.from.last_name then
-      local lname = (msg.from.last_name or '---')
+      lname = (msg.from.last_name or '---')
       end
     if msg.from.username then
-      local username = (msg.from.username or "---")  
+      username = (msg.from.username or "---")  
      end   
     local id = msg.from.id
     local text = name..lname..username
