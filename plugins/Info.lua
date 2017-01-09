7b8c92109a98ab829188ddaab25039c73dae4617
/Seyed <seyedkia27@gmail.com>
2017-01-09 17:48:43 +0330

    Update Info.lua

--- plugins/Info.lua
+++ plugins/Info.lua
@@ -3,11 +3,9 @@ do
     if matches[1]:lower() == "info" then
       if is_sudo(msg) or is_admin1(msg) then
         reply_document(msg.id, "./data/photos/sudo.webp", ok_cb, false)
-      end
-      if is_owner(msg) then
+      elseif is_owner(msg) then
         reply_document(msg.id, "./data/photos/owner.webp", ok_cb, false)
-      end
-      if is_momod(msg) then
+      elseif is_momod(msg) then
         reply_document(msg.id, "./data/photos/momod.webp", ok_cb, false)
       else
         reply_document(msg.id, "./data/photos/member.webp", ok_cb, false)
