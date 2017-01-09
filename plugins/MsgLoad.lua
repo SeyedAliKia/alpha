9371ff8c7c208eb8ef95fbf35143c4572c56ad48
/Seyed <seyedkia27@gmail.com>
2017-01-09 22:20:24 +0330

    Update MsgLoad.lua

--- plugins/MsgLoad.lua
+++ plugins/MsgLoad.lua
@@ -87,6 +87,9 @@ local hash10 = 'strict:'..msg.to.id
 			local is_link_msg = msg.text:match("[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/") or msg.text:match("[Tt][Ll][Gg][Rr][Mm].[Mm][Ee]/") or msg.text:match("[Tt].[Mm][Ee]/") or msg.text:match("[Tt][Ee][Ll][Gg][Rr][Aa][Mm].[Dd][Oo][Gg]/")
 			if is_link_msg and redis:get(hash1) then
 				delete_msg(msg.id, ok_cb, false)
+				delete_msg(msg.id, ok_cb, false)
+				delete_msg(msg.id, ok_cb, false)
+				delete_msg(msg.id, ok_cb, false)	
 				if strict == "yes" or to_chat then
 					kick_user(msg.from.id, msg.to.id)
 				end
