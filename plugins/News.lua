4d3968ba02dc7f17a4d0b2be994760f26725c1ee
/Seyed <seyedkia27@gmail.com>
2017-01-11 14:01:57 +0330

    Update News.lua

--- plugins/News.lua
+++ plugins/News.lua
@@ -1,9 +1,10 @@
 local function run(msg, matches)
 local url = http.request("https://irapi.ir/sports/newspaper.php")
   local jdat = json:decode(url)
-  local text2 = ''
+  local text = ''
+  local random = math.random(1,10)
   for i = 1 , #jdat do
-    text2 = jdat[2].image
+    text2 = jdat[random].image
     end
   return reply_msg(msg.id, text2, ok_cb, false)
 end
