19abadd3420918665adba359cdf47c9cdbe240a2
/Seyed <seyedkia27@gmail.com>
2017-01-11 14:06:46 +0330

    Update News.lua

--- plugins/News.lua
+++ plugins/News.lua
@@ -4,9 +4,10 @@ local url = http.request("https://irapi.ir/sports/newspaper.php")
   local text = ''
   local random = math.random(1,10)
   for i = 1 , #jdat do
-    text2 = jdat[random].image
+    text = jdat[random].image
     end
-  return reply_msg(msg.id, text2, ok_cb, false)
+  local file = download_to_file(text, '@Flooding.jpg')
+  reply_photo(msg.id, file, ok_cb, false)
 end
 
 return {
