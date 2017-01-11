17c89a0a2bae83133d3ae51a8366b942dc365e44
/Seyed <seyedkia27@gmail.com>
2017-01-11 13:51:36 +0330

    Create News.lua

---
+++ plugins/News.lua
@@ -0,0 +1,18 @@
+local function run(msg, matches)
+local url = http.request("https://irapi.ir/sports/newspaper.php")
+  local jdat = json:decode(url)
+  local text2 = ''
+  for i = 1 , #jdat do
+    text2 = text2..i
+    text2 = text2..' ) اسم مبایل : '..jdat[i].image
+    end
+  return reply_msg(msg.id, text2, ok_cb, false)
+end
+
+return {
+  description = "Get New Mobile Phones From Mobile.ir",
+  patterns = {
+    "^([Mm][Oo][Bb][Ii][Ll][Ee])$"
+  },
+  run = run
+}
