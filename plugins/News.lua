92573558c84b4b93c9b215565a24b32ee8e50a65
/Seyed <seyedkia27@gmail.com>
2017-01-11 14:00:20 +0330

    Update News.lua

--- plugins/News.lua
+++ plugins/News.lua
@@ -3,8 +3,7 @@ local url = http.request("https://irapi.ir/sports/newspaper.php")
   local jdat = json:decode(url)
   local text2 = ''
   for i = 1 , #jdat do
-    text2 = text2..i
-    text2 = text2..jdat[2].image
+    text2 = jdat[2].image
     end
   return reply_msg(msg.id, text2, ok_cb, false)
 end
